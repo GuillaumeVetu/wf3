d15be696ac26d580293ea81871f56adeca68734c
Guillaume Vetu <guillaume.escaudain@gmail.com>
2017-11-03 12:42:36 +0100

    ajout du majeur
    prout prout

--- index.php
+++ index.php
@@ -7,9 +7,26 @@
 	<?php
 		$nom = 'Tong';
 		$prenom = 'Tin';
-		$age = '30 ans';
+		$age = '33 ans';
 		
-		echo "Bondour " . $prenom . " " . $nom . " " . $age;
+		echo "Bondour de m'appelle " . $prenom . " " . $nom . " " . $age . " de vie en France depuis pas très très longtemps.";
+		echo "<br/>";
+
+		$ville1 = 'Pyonghiang';
+		$ville2 = 'Paris';
+
+		echo "Je viens de " . $ville1 . " ." . " Maintenant j'habite à ". $ville2 . ".";
+
+		echo "<br/>";
+
+		if ($age >= 18) {
+			echo "Majeur";
+		} elseif ($age >= 15) {
+			echo "Ado";
+		} else {
+			echo "Enfant";
+		}
+		echo "<br/>";
 	?>
 	</body>
 </html>
