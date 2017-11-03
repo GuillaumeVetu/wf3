3da371ffb619cc10ca6cf5021a2247ffdcf502eb
Guillaume Vetu <guillaume.escaudain@gmail.com>
2017-11-03 12:18:22 +0100

    Début de la guerre contre les reptiliens
    C'est un index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Exemple</title>
+	</head>
+	<body>
+	<?php
+		echo "Bonjour il est un script PHP! Vraiment, je ment pas, je vous jure. Antho pense que je suis réptilien au passage. Il se trompe... Ou pas."
+	?>
+	</body>
+</html>
